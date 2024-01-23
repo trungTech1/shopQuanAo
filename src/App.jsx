b0bc9a6aa8d08bead8a5675b02bb64d5d08bb7ca
root <root@DESKTOP-KR6Q0VH>
2024-01-23 13:59:40 +0700

    new

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Homepapes from "./papes/users/homepapes";
+
+function App() {
+  return (
+    <>
+      <Homepapes />
+    </>
+  );
+}
+
+export default App;
